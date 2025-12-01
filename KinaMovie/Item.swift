852ab864aca752550cf2b5028b06b15d2c460d79
shanbe-asl <qadamshoev.s@gmail.com>
2025-12-01 14:46:42 +0500

    Initial Commit

---
+++ KinaMovie/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KinaMovie
+//
+//  Created by Fitrat on 12/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ KinaMovieTests/KinaMovieTests.swift
@@ -0,0 +1,16 @@
+//
+//  KinaMovieTests.swift
+//  KinaMovieTests
+//
+//  Created by Fitrat on 12/1/25.
+//
+
+import Testing
+
+struct KinaMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
